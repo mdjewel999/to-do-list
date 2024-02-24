43cc2ccb7db434e0e568b307f41dbf678ff8a1bf
mdjewel999 <mdjewel878761@gmail.com>
2024-02-25 02:02:09 +0600

    try

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -31,7 +31,7 @@ const Todo = () => {
 
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
-      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>TO-DO APP</h2>
+      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>To-Do List application</h2>
       <div className="flex items-center mb-4">
         <input
           id="addTodoInput"
@@ -45,7 +45,7 @@ const Todo = () => {
           className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
           onClick={handleAddTodoClick}
         >
-          <BsPlus size={20} />
+         <span className='flex items-center justify-center'> <BsPlus size={20} /> Add</span>
         </button>
       </div>
 
